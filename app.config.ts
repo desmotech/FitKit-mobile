dd89451473134405874dedff2a44a84aefd99b1a
Saar Kuriel <desmotechi@protonmail.com>
2026-06-01 18:21:09 +0300

    chore(release): bump version to 1.0.1 for the fixes batch build

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'FitKit',
   slug: 'fitkit',
   scheme: 'fitkit',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   userInterfaceStyle: 'automatic',
